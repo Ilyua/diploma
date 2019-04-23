3bd69f302254a9ab9ecd3b2281a37db9255dd8fa
Ilya Chumak <i.chumak@bi.zone>
2019-04-23 11:50:20 +0300

    3

--- thasrk.sh
+++ thasrk.sh
@@ -1,3 +1,3 @@
 touch ./input_folder/capture-output.pcap
-chmod ./input_folder/capture-output.pcap
+chmod 777 ./input_folder/capture-output.pcap
 tshark -i enp0s3 -w ./input_folder/capture-output.pcap 

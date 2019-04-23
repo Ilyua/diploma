d970e6f087fe2a483c4895216101a42ff5f6a53c
Ilya Chumak <i.chumak@bi.zone>
2019-04-23 11:26:09 +0300

    tail

--- predict.py
+++ predict.py
@@ -38,7 +38,33 @@ columns = ['Destination Port', 'Flow Duration', 'Total Fwd Packets',
 
 columns_to_drop =['Flow ID', 'Src IP', 'Src Port', 'Dst IP', 'Protocol','Timestamp' ]
 
-
+initial_columns= [' Destination Port', ' Flow Duration', ' Total Fwd Packets',
+       ' Total Backward Packets', 'Total Length of Fwd Packets',
+       ' Total Length of Bwd Packets', ' Fwd Packet Length Max',
+       ' Fwd Packet Length Min', ' Fwd Packet Length Mean',
+       ' Fwd Packet Length Std', 'Bwd Packet Length Max',
+       ' Bwd Packet Length Min', ' Bwd Packet Length Mean',
+       ' Bwd Packet Length Std', 'Flow Bytes/s', ' Flow Packets/s',
+       ' Flow IAT Mean', ' Flow IAT Std', ' Flow IAT Max', ' Flow IAT Min',
+       'Fwd IAT Total', ' Fwd IAT Mean', ' Fwd IAT Std', ' Fwd IAT Max',
+       ' Fwd IAT Min', 'Bwd IAT Total', ' Bwd IAT Mean', ' Bwd IAT Std',
+       ' Bwd IAT Max', ' Bwd IAT Min', 'Fwd PSH Flags', ' Bwd PSH Flags',
+       ' Fwd URG Flags', ' Bwd URG Flags', ' Fwd Header Length',
+       ' Bwd Header Length', 'Fwd Packets/s', ' Bwd Packets/s',
+       ' Min Packet Length', ' Max Packet Length', ' Packet Length Mean',
+       ' Packet Length Std', ' Packet Length Variance', 'FIN Flag Count',
+       ' SYN Flag Count', ' RST Flag Count', ' PSH Flag Count',
+       ' ACK Flag Count', ' URG Flag Count', ' CWE Flag Count',
+       ' ECE Flag Count', ' Down/Up Ratio', ' Average Packet Size',
+       ' Avg Fwd Segment Size', ' Avg Bwd Segment Size',
+       ' Fwd Header Length.1', 'Fwd Avg Bytes/Bulk', ' Fwd Avg Packets/Bulk',
+       ' Fwd Avg Bulk Rate', ' Bwd Avg Bytes/Bulk', ' Bwd Avg Packets/Bulk',
+       'Bwd Avg Bulk Rate', 'Subflow Fwd Packets', ' Subflow Fwd Bytes',
+       ' Subflow Bwd Packets', ' Subflow Bwd Bytes', 'Init_Win_bytes_forward',
+       ' Init_Win_bytes_backward', ' act_data_pkt_fwd',
+       ' min_seg_size_forward', 'Active Mean', ' Active Std', ' Active Max',
+       ' Active Min', 'Idle Mean', ' Idle Std', ' Idle Max', ' Idle Min',
+       ' Label']
 
 def process_data(df):
     def f1(x):
@@ -109,51 +135,68 @@ def tail(fin):
             fin.seek(where)
         else:
             yield line
-            
+
 def main():
     with open('./output_folder/capture-output.pcap_Flow.csv', 'r') as fin:
-        for line in readlines_then_tail(fin):
-            print (line.strip())
-
-
-i=0
-
-while True:
-
-    try:
+        for i,line in enumerate(readlines_then_tail(fin)):
+            
+            print('FLOW ',i)
         
 
-        
-        df= pd.read_csv('./output_folder/capture-output.pcap_Flow.csv')
-        print(df.columns)
-        
-        i=i+1
-        print(i)
-        
+            chunk = pd.read_csv(StringIO(line))
+            chunk.columns = initial_columns
 
-        # chunk.drop(columns_to_drop,axis=1,inplace=True)
-        # print(chunk)
-        # chunk.columns = columns
-        # chunk = process_data(chunk)
+            chunk.drop(columns_to_drop,axis=1,inplace=True)
+            print(chunk)
+            chunk.columns = columns
+            chunk = process_data(chunk)
 
-        # #     print(chunk.columns)
-        # new_chunk = chunk.copy()
+            #     print(chunk.columns)
+            new_chunk = chunk.copy()
 
-        # new_chunk['anomality'] = detector.decision_function(chunk.drop(['Label'],axis=1))
+            new_chunk['anomality'] = detector.decision_function(chunk.drop(['Label'],axis=1))
 
 
-        # result = model.predict(new_chunk.drop(['Label'],axis=1))
+            result = model.predict(new_chunk.drop(['Label'],axis=1))
+
+
+            if result == 1:
+               print(new_chunk['Destination Port'])
+               print('DETECTED',datetime.datetime.now())
+
+main()
+
+    # i=0
+
+    #     row 
+        
+#         i=i+1
+#         print(i)
+        
+
+#         # chunk.drop(columns_to_drop,axis=1,inplace=True)
+#         # print(chunk)
+#         # chunk.columns = columns
+#         # chunk = process_data(chunk)
+
+#         # #     print(chunk.columns)
+#         # new_chunk = chunk.copy()
+
+#         # new_chunk['anomality'] = detector.decision_function(chunk.drop(['Label'],axis=1))
+
+
+#         # result = model.predict(new_chunk.drop(['Label'],axis=1))
         
         
-        # if result == 1:
-        #     print(new_chunk['Destination Port'])
-        #     print('DETECTED',datetime.datetime.now())
+#         # if result == 1:
+#         #     print(new_chunk['Destination Port'])
+#         #     print('DETECTED',datetime.datetime.now())
         
         
-    except Exception as e:
-        # print(e)
-        print('\rwaiting for new connections',end='')
-        pass
+#     except Exception as e:
+#         # print(e)
+#         print('\rwaiting for new connections',end='')
+#         pass
 
 
 
